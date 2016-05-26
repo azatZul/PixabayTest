19b2d0513890a9fa3d679a31c938265258f845f7
azat <azat.zulkarnyaev@gmail.com>
2016-05-26 12:45:06 +0300

    Создал проект

---
+++ PixabayTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  PixabayTest
+//
+//  Created by azat on 26/05/16.
+//  Copyright (c) 2016 Azat Zul. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
---
+++ PixabayTest/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  PixabayTest
+//
+//  Created by azat on 26/05/16.
+//  Copyright (c) 2016 Azat Zul. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
